bf56621b848b5ff4986dec8c58c51773801508c9
huydo <od07@htklabs.com>
2016-08-05 10:16:54 +0700

    use skellington

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,5 @@
 var Request          = require('request')
-var slack           = require('../controllers/botkit')
+// var slack           = require('../controllers/botkit')
 
 // frontend routes =========================================================
 module.exports = function(app) {
@@ -83,7 +83,7 @@ module.exports = function(app) {
           startBot(team)
           res.send("Your bot has been installed")
 
-          saveUser(auth, identity)
+          // saveUser(auth, identity)
         }
         catch(e){
           console.log(e)
@@ -95,40 +95,41 @@ module.exports = function(app) {
   var startBot = function(team){
     console.log(team.name + " start bot")
 
-    slack.connect(team)
+    // slack.connect(team)
+    require('skellington')(team);
   }
 
-  var saveUser = function(auth, identity){
-    // what scopes did we get approved for?
-    var scopes = auth.scope.split(/\,/);
+  // var saveUser = function(auth, identity){
+  //   // what scopes did we get approved for?
+  //   var scopes = auth.scope.split(/\,/);
 
-    slack.controller.storage.users.get(identity.user_id, function(err, user) {
-      isnew = false;
-      if (!user) {
-          isnew = true;
-          user = {
-              id: identity.user_id,
-              access_token: auth.access_token,
-              scopes: scopes,
-              team_id: identity.team_id,
-              user: identity.user,
-          };
-      }
-      slack.controller.storage.users.save(user, function(err, id) {
-        if (err) {
-          console.log('An error occurred while saving a user: ', err);
-          slack.controller.trigger('error', [err]);
-        }
-        else {
-          if (isnew) {
-            console.log("New user " + id.toString() + " saved");
-          }
-          else {
-            console.log("User " + id.toString() + " updated");
-          }
-          console.log("================== END TEAM REGISTRATION ==================")
-        }
-      });
-    });
-  }
+  //   slack.controller.storage.users.get(identity.user_id, function(err, user) {
+  //     isnew = false;
+  //     if (!user) {
+  //         isnew = true;
+  //         user = {
+  //             id: identity.user_id,
+  //             access_token: auth.access_token,
+  //             scopes: scopes,
+  //             team_id: identity.team_id,
+  //             user: identity.user,
+  //         };
+  //     }
+  //     slack.controller.storage.users.save(user, function(err, id) {
+  //       if (err) {
+  //         console.log('An error occurred while saving a user: ', err);
+  //         slack.controller.trigger('error', [err]);
+  //       }
+  //       else {
+  //         if (isnew) {
+  //           console.log("New user " + id.toString() + " saved");
+  //         }
+  //         else {
+  //           console.log("User " + id.toString() + " updated");
+  //         }
+  //         console.log("================== END TEAM REGISTRATION ==================")
+  //       }
+  //     });
+  //   });
+  // }
 }
